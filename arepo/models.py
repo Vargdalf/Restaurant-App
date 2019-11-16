59526d286d154d14057e8df58fd1c615edee407f
Kacper Mieszala <24653160+Vargdalf@users.noreply.github.com>
2019-11-16 13:31:35 +0100

    Made models

---
+++ arepo/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.db.models import CASCADE
+
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField
+    image = models.ImageField(default=None)
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    achievements = models.ManyToManyField(Achievement)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Order(models.Model):
+    table = models.IntegerField
+    employee = models.ForeignKey(Employee, on_delete=CASCADE)
+    tip = models.IntegerField
+    dishes = models.ManyToManyField(Dish)
