f94f4c59f0c921122ea3a0949de3258e3bcdf6fb
Kacper Mieszala <24653160+Vargdalf@users.noreply.github.com>
2019-11-17 12:10:27 +0100

    Home Page Added

--- arepo/urls.py
+++ arepo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from arepo.views import PanelView
+from arepo.views import PanelView, HomePageView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('', HomePageView.as_view(), name='home'),
     path('panel/', PanelView.as_view(), name='panel'),
 ]
--- arepo/views.py
+++ arepo/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
 class PanelView(TemplateView):
     template_name = 'panel.html'
