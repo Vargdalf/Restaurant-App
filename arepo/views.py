7998157ea3d45476c1226b9c215dc56c77b48397
Kacper Mieszala <24653160+Vargdalf@users.noreply.github.com>
2019-11-24 15:03:13 +0100

    Stats

--- arepo/views.py
+++ arepo/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.shortcuts import render
 
 from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView
 
