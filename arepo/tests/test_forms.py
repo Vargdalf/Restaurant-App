e932feb152d720bb24b984b1eef3738b20267292
Kacper Mieszala <24653160+Vargdalf@users.noreply.github.com>
2019-11-27 01:16:08 +0100

    Updated forms tests and started adding tests for views

--- arepo/tests/test_forms.py
+++ arepo/tests/test_forms.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
 
+from model_bakery import baker
+
 from arepo.forms import NewOrderForm
 
 
 class NewOrderFormTest(TestCase):
+
+    def setUp(self):
+        self.dishes = baker.make('Dish', _quantity=1)
+
+    def test_NewOrder_form_valid(self):
+        form = NewOrderForm(data={
+            'table': 1,
+            'dishes': self.dishes,
+        })
+        self.assertTrue(form.is_valid())
 
     def test_form_validation_for_blank_fields(self):
         form = NewOrderForm(data={
