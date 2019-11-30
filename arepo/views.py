c94a616c0f272dccb6fff538d6b4ae414a4a5617
Kacper Mieszala <24653160+Vargdalf@users.noreply.github.com>
2019-11-30 14:06:19 +0100

    Front page and panel frontend added

--- arepo/views.py
+++ arepo/views.py
@@ -88,7 +88,9 @@ class StatView(LoginRequiredMixin, TemplateView):
             for order in self.all_orders:
                 self.total_value += order.get_full_price()
 
+        content = {'total_tips': total_tips, 'daily_tips': daily_tips, 'today': self.today,
+                   'monthly_tips': monthly_tips, 'weekly_tips': weekly_tips, 'dish_counter': self.dish_counter,
+                   'all_orders': self.all_orders, 'total_value': self.total_value}
+
         return render(request, self.template_name,
-                      {'total_tips': total_tips, 'daily_tips': daily_tips, 'today': self.today,
-                       'monthly_tips': monthly_tips, 'weekly_tips': weekly_tips, 'dish_counter': self.dish_counter,
-                       'all_orders': self.all_orders, 'total_value': self.total_value}, )
+                      content, )
